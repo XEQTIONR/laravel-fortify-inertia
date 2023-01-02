1f26a2c1a4c5be9f8e5edd0cb5e32cec80183866
Ishtehar Hussain <ishtehar@alley.com>
2023-01-02 22:18:42 +0600

    Install Roboto font and Material icons.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,10 @@
 import './bootstrap';
 
 import "../css/app.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import getLodash from "lodash/get";
 import eachRightLodash from "lodash/eachRight";
