df1e7c0db62a02c367b25f6f61aeefb02e039b75
Ishtehar Hussain <ishtehar@alley.com>
2023-01-05 20:53:54 +0600

    Clean up

--- resources/js/Pages/Admin/Dashboard2.jsx
+++ resources/js/Pages/Admin/Dashboard2.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import {
+    AppBar,
+    Box,
+    createTheme,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    Typography,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar
+} from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from "@mui/icons-material/Menu";
-import {createTheme, IconButton} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 240;
 
