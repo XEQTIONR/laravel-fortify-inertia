e7ad19e4ad5de74dbbdbad907b1e2ca8a0680855
Ishtehar Hussain <ishtehar@alley.com>
2023-01-09 16:29:10 +0600

    Amin dashboard view and nav

---
+++ resources/js/Components/Admin/Nav.jsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from 'react';
+import {
+    AppBar,
+    Box,
+    createTheme,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    Typography,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar
+} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+
+const drawerWidth = 240;
+
+const theme = createTheme({
+    transitions: {
+        easing: {
+            // The sharp curve is used by objects that may return to the screen at any time.
+            sharp: 'cubic-bezier(0.4, 0, 0.6, 1)',
+        },
+        duration: {
+            // recommended when something is leaving screen
+            leavingScreen: 195,
+        },
+    },
+});
+
+export default function Dashboard({ children, navLinks }) {
+
+    const [ show, setShow ] = useState(false);
+    const [ mainWidth, setMainWidth ] = useState(window.innerWidth);
+    const [ menuType, setMenuType ] = useState((window.innerWidth > 800) ? 'persistent' : 'temporary');
+    useEffect(() => {
+        function handleResize() {
+            setMainWidth(window.innerWidth);
+            if (window.innerWidth <= 800 && menuType === 'persistent' ) {
+                setMenuType('temporary');
+            } else if (window.innerWidth > 800 && menuType === 'temporary' ) {
+                setMenuType('persistent');
+            }
+        }
+        window.addEventListener('resize', handleResize)
+    });
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+                <Toolbar className="px-1">
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        color="inherit"
+                        onClick={() => { setShow( !show )}}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Clipped drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                open={show}
+                variant={menuType}
+                onClose={() => (mainWidth < 800) && setShow(false)}
+                sx={{
+                    width: show ? drawerWidth : 0,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+            >
+                <Toolbar />
+                <Box sx={{ overflow: 'auto' }}>
+                    <List>
+                        {navLinks.map((item, index) => (
+                            <ListItem key={item.label} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.label} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+            <Box
+                style={{
+                    transition: theme.transitions.create("all", {
+                        easing: theme.transitions.easing.sharp,
+                        duration: theme.transitions.duration.leavingScreen }),
+                    width: (show && menuType === 'persistent') ? mainWidth - drawerWidth : mainWidth,
+                }}
+                component="main"
+                sx={{ flexShrink: 0, p: 3 }}
+            >
+                <Toolbar />
+                { children }
+            </Box>
+        </Box>
+    );
+}
---
+++ resources/js/Components/data/AdminNavItems.jsx
@@ -0,0 +1,27 @@
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import ListIcon from "@mui/icons-material/List";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+
+export default [
+    {
+        label: 'Products',
+        icon: <FastfoodIcon />,
+        link: '#'
+    },
+    {
+        label: 'Orders',
+        icon: <ListIcon />,
+        link: '#'
+    },
+    {
+        label: 'Customers',
+        icon: <RestaurantIcon />,
+        link: '#'
+    },
+    {
+        label: 'Suppliers',
+        icon: <SupervisedUserCircleIcon />,
+        link: '#'
+    },
+]
