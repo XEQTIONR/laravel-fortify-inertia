0260ed00bbacb4689de865d93d0f1fc12098cff9
Ishtehar Hussain <ishtehar@alley.com>
2023-01-17 13:00:02 +0600

    Refactor out add product form component.

--- resources/js/Pages/Admin/Products.jsx
+++ resources/js/Pages/Admin/Products.jsx
@@ -1,56 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useForm } from '@inertiajs/inertia-react';
 import Nav from '@/Components/Admin/Nav';
+import AddProductForm from '@/Components/Admin/AddProductForm';
 import navItems from  '@/Components/data/AdminNavItems';
 import { DataGrid } from '@mui/x-data-grid';
 
 import { Add, Delete, Edit } from "@mui/icons-material";
-import {
-    Box,
-    Button,
-    Divider,
-    Fab,
-    FormControl,
-    Input,
-    InputAdornment,
-    InputLabel,
-    Tooltip,
-    Modal,
-    Paper,
-    Select,
-    Stack,
-    Typography,
-    TextField,
-    useMediaQuery,
-} from '@mui/material'
-
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-
-//import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { styled } from '@mui/material/styles';
-
-const AddProductForm = styled( Paper )(({ theme }) => ({
-    [theme.breakpoints.down('sm')]: {
-        width: '90%',
-        mx: 2
-    },
-    [theme.breakpoints.down('md')]: {
-        minWidth: '50vw',
-        maxWidth: 450
-    },
-    [theme.breakpoints.up('md')]: {
-        width: 450
-    },
-}));
+import { Box, Fab, Tooltip, Modal } from '@mui/material'
 
 export default function Products({ products, uom }) {
 
@@ -59,29 +14,6 @@ export default function Products({ products, uom }) {
     const [rows, setRows] = useState(products);
     const [ selected, setSelected ] =  useState([]);
     const [ showAddForm, setShowAddForm ] = useState(false);
-    const [ uploadedImageUrl, setUploadedImageUrl ] = useState(null);
-
-    const { data, setData, post, processing, errors } = useForm({
-        'english_name' : null,
-        'bangla_name' : null,
-        'uom' : null,
-        'current_selling_price' : null,
-        'image' : null,
-    })
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        post( route('admin.products.store'), {
-            //forceFormData: true,
-            onError: (err) => {
-                console.log('submit error');
-                console.log(err);
-            }
-        })
-    }
-
-
-    const blockButtonQuery = useMediaQuery('(max-width:600px)');
 
     const columns = [
         {
@@ -173,138 +105,7 @@ export default function Products({ products, uom }) {
                     alignItems: 'center'
                 }}
             >
-                <AddProductForm elevation={24}>
-                    <form onSubmit={handleSubmit}>
-                    <Stack spacing={2}
-                           sx={{ p: 5, backgroundColor: 'white', borderRadius: 1 }}
-                    >
-                        <Typography align="center" variant="h6" sx={{ fontWeight: 'bold' }}>Add a new product</Typography>
-                        <Divider />
-                        <TextField
-                            onChange={({ target }) => setData('english_name', target.value)}
-                            required
-                            id="outlined-required"
-                            label="Name"
-                            variant="standard"
-                        />
-                        <TextField
-                            onChange={({target}) => setData('bangla_name', target.value)}
-                            required
-                            id="outlined-required"
-                            label="Name (Bangla)"
-                            variant="standard"
-                        />
-                        {/*<FormControl fullWidth sx={{ m: 1 }}>*/}
-                            {/*<InputLabel htmlFor="outlined-adornment-amount">Current Selling Price</InputLabel>*/}
-                            <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
-                                <Typography sx={{ mr: 1 }}>৳</Typography>
-                                <TextField
-                                    onChange={({target}) => setData('current_selling_price', target.value)}
-                                    required
-                                    id="outlined-required"
-                                    label="Current Selling Price"
-                                    variant="standard"
-                                />
-                            </Box>
-                            {/*<Input*/}
-                            {/*    onChange={({target}) => setData('current_selling_price', target.value)}*/}
-                            {/*    type="number"*/}
-                            {/*    inputProps={{min: 0, step: 0.01, style: { textAlign: 'right' }}}*/}
-                            {/*    id="outlined-adornment-amount"*/}
-                            {/*    startAdornment={<InputAdornment position="start">৳</InputAdornment>}*/}
-                            {/*    label="Amount"*/}
-                            {/*/>*/}
-                        {/*</FormControl>*/}
-                        <FormControl variant="standard" sx={{ m: 1 }}>
-                            <InputLabel id="demo-simple-select-standard-label">Unit Of Measure</InputLabel>
-                            <Select
-                                onChange={ ({target}) => setData('uom', target.value) }
-                                id="select-input-id"
-                                label="Age"
-                            >
-                                <MenuItem value="none">None</MenuItem>
-                                <MenuItem value="ten">Ten</MenuItem>
-                                <MenuItem value="twenty">Twenty</MenuItem>
-                                <MenuItem value="thirty">Thirty</MenuItem>
-                            </Select>
-                        </FormControl>
-
-                        <Stack spacing={2} sx={{ height: 1/3}}>
-                            <Typography>Product Image</Typography>
-                            <Stack direction="row" sx={{ height: 1/2 }}>
-                                <Box sx={{ width: 1/2 }}>
-                                    {   uploadedImageUrl
-                                        ? (
-                                            <Button
-                                                variant="outlined"
-                                                onClick={() => setUploadedImageUrl(null)}
-                                            >
-                                                Remove Image
-                                            </Button>
-                                        )
-                                        : (
-                                            <Button sx={{ mr: 2 }} variant="outlined" component="label">
-                                                Select Image
-                                                <input hidden accept="image/*" type="file" onChange={(e) => {
-                                                    setData('image', e.target.files[0])
-                                                    setUploadedImageUrl(URL.createObjectURL(e.target.files[0]))
-                                                }} />
-                                            </Button>
-                                        )
-                                    }
-                                </Box>
-                                <Box style={{
-                                    width: "50%",
-                                }}>
-                                    <Box sx={{
-                                        backgroundColor: "#eee",
-                                        width: "100%",
-                                        height: "0",
-                                        paddingTop: "100%",
-                                        position: "relative"
-                                    }}>
-                                        {   uploadedImageUrl
-                                            ? <img
-                                                style={{
-                                                    position: 'absolute',
-                                                    top: "50%",
-                                                    left: "50%",
-                                                    transform: "translate(-50%, -50%)",
-                                                }}
-                                                src={uploadedImageUrl}
-                                            /> :
-                                            (<Box sx={{
-                                            position: 'absolute',
-                                            top: "50%",
-                                            left: "50%",
-                                            transform: "translate(-50%, -50%)",
-                                            display: "flex",
-                                            flexDirection: "column",
-                                            justifyContent: "center",
-                                            alignItems: "center",
-                                            }}>
-                                                <InsertPhotoIcon sx={{
-                                                    fontSize: '3rem',
-                                                    color: '#999',
-                                                }} />
-                                                <Typography color="#888" align="center" variant="button">No Image Selected</Typography>
-                                            </Box>)
-                                        }
-                                    </Box>
-                                </Box>
-                            </Stack>
-                        </Stack>
-                        <Stack sx={{ pt: 2 }}>
-                        <Button
-                            type="submit"
-                            variant="contained"
-                        >
-                            Submit
-                        </Button>
-                        </Stack>
-                    </Stack>
-                    </form>
-                </AddProductForm>
+                <AddProductForm uom={uom} />
             </Modal>
             <Box
                 className="flex flex-row-reverse justify-between items-end"
